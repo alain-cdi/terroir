2a99cbfab06d1cd97025cc8f89e9b2a84678ed64
Crozes <alain.crozes2@orange.fr>
2012-10-15 15:20:10 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Terroir::Application.config.secret_token = 'cbbeab3148ee41c9594fa46862d85d514bf40daf17dbe917c580f3404a2f96d82e09dc19ddf553f616d7ff5573193a383bbe96ff2bc4262dc277286fc56bf989'
